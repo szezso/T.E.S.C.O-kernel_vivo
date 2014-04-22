f6d43c60e08e0f8a2b65b029b2a720c60c2d3002
szezso <szezso@szezso-kitchen.(none)>
2014-04-22 11:45:44 +0200

    board-vivo: #ifdef reserve_memory_for

--- arch/arm/mach-msm/board-vivo.c
+++ arch/arm/mach-msm/board-vivo.c
@@ -5812,6 +5812,7 @@ static void __init size_pmem_devices(void)
 }
 
 #ifdef CONFIG_ANDROID_PMEM
+#ifndef CONFIG_MSM_MULTIMEDIA_USE_ION
 static void __init reserve_memory_for(struct android_pmem_platform_data *p)
 {
 	if (p->size > 0) {
@@ -5819,6 +5820,7 @@ static void __init reserve_memory_for(struct android_pmem_platform_data *p)
 		msm7x30_reserve_table[p->memory_type].size += p->size;
 	}
 }
+#endif
 #endif
 
 static void __init reserve_pmem_memory(void)
