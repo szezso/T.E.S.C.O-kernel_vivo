3db2a2f3574e515d4db27660dec38e4f00ccfee8
szezso <szezso@szezso-kitchen.(none)>
2014-06-07 13:54:38 +0200

    Defconfig: Enable zRAM & XVMALLOC, use Linaro 4.9 GCC for building

--- arch/arm/mach-msm/board-vivow.c
+++ arch/arm/mach-msm/board-vivow.c
@@ -56,16 +56,16 @@
 #include <linux/usb/msm_hsusb.h>
 #endif
 #include <mach/msm_spi.h>
-#include <mach/qdsp5v2/msm_lpa.h>
+#include <mach/qdsp5v2_2x/msm_lpa.h>
 #include <mach/dma.h>
 #include <linux/android_pmem.h>
 #include <linux/input/msm_ts.h>
 #include <mach/pmic.h>
 
 #include <mach/rpc_pmapp.h>
-#include <mach/qdsp5v2/aux_pcm.h>
-#include <mach/qdsp5v2/mi2s.h>
-#include <mach/qdsp5v2/audio_dev_ctl.h>
+#include <mach/qdsp5v2_2x/aux_pcm.h>
+#include <mach/qdsp5v2_2x/mi2s.h>
+#include <mach/qdsp5v2_2x/audio_dev_ctl.h>
 #include <mach/htc_battery.h>
 #include <linux/tps65200.h>
 #include <mach/rpc_server_handset.h>
@@ -99,13 +99,13 @@
 #ifdef CONFIG_SERIAL_MSM_HS_PURE_ANDROID
 #include <mach/bcm_bt_lpm.h>
 #endif
-#include <mach/qdsp5v2/mi2s.h>
-#include <mach/qdsp5v2/audio_dev_ctl.h>
+#include <mach/qdsp5v2_2x/mi2s.h>
+#include <mach/qdsp5v2_2x/audio_dev_ctl.h>
 #include <mach/sdio_al.h>
 #include "smd_private.h"
 #include <linux/bma150.h>
 #include "board-vivow.h"
-#include <linux/tpa2051d3.h>
+#include <mach/tpa2051d3.h>
 #include "board-msm7x30-regulator.h"
 #include <mach/board_htc.h>
 #include <mach/htc_sleep_clk.h>
